7c139f9ff969d458fab6c4205486f9f4a9cfeb3a
pishon313 <pishon313@gmail.com>
2022-10-30 22:31:21 +0900

    #VideoInput, PhotoOutput setting

--- customCamera/CameraViewController.swift
+++ customCamera/CameraViewController.swift
@@ -23,7 +23,53 @@ class CameraViewController: ViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        previewView.session = captureSession
+        
+        captureSession.sessionPreset = .photo
+        captureSession.beginConfiguration()
+        
+        // Add Video Input
+        do {
+            var defaultVideoDevice: AVCaptureDevice?
+            if let dualCameraDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+                defaultVideoDevice = dualCameraDevice
+            } else if let backCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+                defaultVideoDevice = backCameraDevice
+            } else if let frontCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
+                defaultVideoDevice = frontCameraDevice
+            }
+            
+            guard let camera = defaultVideoDevice else {
+                captureSession.commitConfiguration()
+                return
+            }
+            
+            let videoDeviceInput = try AVCaptureDeviceInput(device: camera)
+            
+            if captureSession.canAddInput(videoDeviceInput) {
+                captureSession.addInput(videoDeviceInput)
+                self.videoDeviceInput = videoDeviceInput
+            } else {
+                captureSession.commitConfiguration()
+                return
+            }
+        } catch {
+            captureSession.commitConfiguration()
+            return
+        }
+        
+        
+        // Add photo output
+        photoOutput.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+        if captureSession.canAddOutput(photoOutput) {
+            captureSession.addOutput(photoOutput)
+        } else {
+            captureSession.commitConfiguration()
+            return
+        }
+        
+        captureSession.commitConfiguration()
     }
 
 
